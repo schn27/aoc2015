3417e96261d76da886d185779aa03c3617f9a002
schn27 <schn27@gmail.com>
2019-11-20 22:42:33 +0300

    [wip]

---
+++ 22.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function calc() {
+	let boss = input.match(/\d+/g).map(Number);
+
+	return "part1" + ", " + "part2";
+}
+
+const input = `Hit Points: 55
+Damage: 8`;
---
+++ 24.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function calc() {
+	const packages = input.match(/\d+/g).map(Number).sort((a, b) => b - a);
+	const groupW = packages.reduce((s, e) => s + e) / 3;
+
+	
+
+	return "part1" + ", " + "part2";
+}
+
+const input = `1
+2
+3
+5
+7
+13
+17
+19
+23
+29
+31
+37
+41
+43
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+103
+107
+109
+113`;
